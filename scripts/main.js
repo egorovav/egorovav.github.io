1f8db6176a80ab194e6b1752f0f3e7121a155c33
egorov_av <egorov_av@mail.ru>
2019-01-20 22:13:28 +0300

    Add discrete math

---
+++ scripts/main.js
@@ -0,0 +1,3 @@
+var refs = document.querySelector('p');
+var contentString = '<div id="content"><h2>Custom info window</h2><p>This is a cool custom info window.</p></div>';
+refs.addEventListener('mouseover', );
